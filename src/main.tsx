4c0f52aa1283b4d514f67f7dce415b6e2c1ad998
gaeundev <gaeundev@gmail.com>
2022-10-07 14:30:37 +0900

    기본 세팅 및 사용할 페이지 작성

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+
+import Root from './pages/Root/Root';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+        <Root />
+    </React.StrictMode>
+);
---
+++ src/pages/About/About.tsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <>
+            <h1>about</h1>
+            <button>go main</button>
+        </>
+    );
+};
+
+export default About;
